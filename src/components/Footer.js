a6f1acf57680c56b9c7c46275baef8491f8540f7
Ben <benlinux915@gmail.com>
2021-03-09 14:49:28 -0500

    added generic copy to header and footer for basic styling, repositioned overlay components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ import '../styles/Footer.scss';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">Footer</div>
+      <div className="footer-logos">
+        <div className="org-logo"></div>
+        <div className="big-red">C</div>
+      </div>
+      <div className="footer-text">
+        <p>Blurb about the funding for the project and partnerships with the logos to the left.</p>
+        <p>Any kind of legal disclaimers or copyright information that needs to be addressed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar efficitur dolor sit amet tristique. Pellentesque convallis risus ante, ut fermentum ipsum tempor non. Aliquam vehicula erat urna, ut pulvinar quam fermentum nec. Curabitur viverra ipsum ipsum, id pharetra sapien molestie a. Cras efficitur ut magna sed tincidunt. Nunc id bibendum dolor, in ullamcorper libero. Donec lobortis erat ex, ac sodales orci commodo at. Sed sit amet leo ac erat pharetra cursus vitae a felis. Quisque semper nibh ac eleifend tincidunt. Vestibulum vel metus ipsum. </p>
+        <p>&#169; XXXX-XXXX Cornell University</p>
+      </div>
     </footer>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,16 @@ import '../styles/Header.scss';
 const Header = () => {
   return (
     <header className="header">
-      Header
+      <div className="org-logo"></div>
+      <div className="site-info">
+        <div className="site-org">Organization Name</div>
+        <div className="site-description">a service of the Some-team-or-another</div>
+      </div>
+      <nav className="nav">
+        <div className="link1">Link 1</div>
+        <div className="link2">Link 2</div>
+        <div className="link3">Link 3</div>
+      </nav>
     </header>
   );
 };
